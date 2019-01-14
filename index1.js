571a131272ffa5dffdf519357b56473b0cf9fe1d
bruno1407 <bruno-3232@hotmail.fr>
2019-01-14 16:40:50 +0100

    First commit

---
+++ index1.js
@@ -0,0 +1,215 @@
+'use strict';
+
+//list of bats
+//useful for ALL 5 steps
+//could be an array of objects that you fetched from api or database
+const bars = [{
+  'id': 'f944a3ff-591b-4d5b-9b67-c7e08cba9791',
+  'name': 'freemousse-bar',
+  'pricePerHour': 50,
+  'pricePerPerson': 20
+}, {
+  'id': '165d65ec-5e3f-488e-b371-d56ee100aa58',
+  'name': 'solera',
+  'pricePerHour': 100,
+  'pricePerPerson': 40
+}, {
+  'id': '6e06c9c0-4ab0-4d66-8325-c5fa60187cf8',
+  'name': 'la-poudriere',
+  'pricePerHour': 250,
+  'pricePerPerson': 80
+}];
+
+//list of current booking events
+//useful for ALL steps
+//the time is hour
+//The price is updated from step 1 and 2
+//The commission is updated from step 3
+//The options is useful from step 4
+const events = [{
+  'id': 'bba9500c-fd9e-453f-abf1-4cd8f52af377',
+  'booker': 'esilv-bde',
+  'barId': 'f944a3ff-591b-4d5b-9b67-c7e08cba9791',
+  'time': 4,
+  'persons': 8,
+  'options': {
+    'deductibleReduction': false
+  },
+  'price': 0,
+  'commission': {
+    'insurance': 0,
+    'treasury': 0,
+    'privateaser': 0
+  }
+}, {
+  'id': '65203b0a-a864-4dea-81e2-e389515752a8',
+  'booker': 'societe-generale',
+  'barId': '165d65ec-5e3f-488e-b371-d56ee100aa58',
+  'time': 8,
+  'persons': 30,
+  'options': {
+    'deductibleReduction': true
+  },
+  'price': 0,
+  'commission': {
+    'insurance': 0,
+    'treasury': 0,
+    'privateaser': 0
+  }
+}, {
+  'id': '94dab739-bd93-44c0-9be1-52dd07baa9f6',
+  'booker': 'otacos',
+  'barId': '6e06c9c0-4ab0-4d66-8325-c5fa60187cf8',
+  'time': 5,
+  'persons': 80,
+  'options': {
+    'deductibleReduction': true
+  },
+  'price': 0,
+  'commission': {
+    'insurance': 0,
+    'treasury': 0,
+    'privateaser': 0
+  }
+}];
+
+//list of actors for payment
+//useful from step 5
+const actors = [{
+  'eventId': 'bba9500c-fd9e-453f-abf1-4cd8f52af377',
+  'payment': [{
+    'who': 'booker',
+    'type': 'debit',
+    'amount': 0
+  }, {
+    'who': 'bar',
+    'type': 'credit',
+    'amount': 0
+  }, {
+    'who': 'insurance',
+    'type': 'credit',
+    'amount': 0
+  }, {
+    'who': 'treasury',
+    'type': 'credit',
+    'amount': 0
+  }, {
+    'who': 'privateaser',
+    'type': 'credit',
+    'amount': 0
+  }]
+}, {
+  'eventId': '65203b0a-a864-4dea-81e2-e389515752a8',
+  'payment': [{
+    'who': 'booker',
+    'type': 'debit',
+    'amount': 0
+  }, {
+    'who': 'bar',
+    'type': 'credit',
+    'amount': 0
+  }, {
+    'who': 'insurance',
+    'type': 'credit',
+    'amount': 0
+  }, {
+    'who': 'treasury',
+    'type': 'credit',
+    'amount': 0
+  }, {
+    'who': 'privateaser',
+    'type': 'credit',
+    'amount': 0
+  }]
+}, {
+  'eventId': '94dab739-bd93-44c0-9be1-52dd07baa9f6',
+  'payment': [{
+    'who': 'booker',
+    'type': 'debit',
+    'amount': 0
+  }, {
+    'who': 'bar',
+    'type': 'credit',
+    'amount': 0
+  }, {
+    'who': 'insurance',
+    'type': 'credit',
+    'amount': 0
+  }, {
+    'who': 'treasury',
+    'type': 'credit',
+    'amount': 0
+  }, {
+    'who': 'privateaser',
+    'type': 'credit',
+    'amount': 0
+  }]
+}];
+
+console.log(bars);
+console.log(events);
+console.log(actors);
+
+
+for(var i = 0 ; i < events.length ; i++)
+{
+  for(var j = 0 ; j < bars.length ; j++)
+  {
+    if(events[i].barId == bars[j].id)
+    {
+      var price_c = bars[j].pricePerPerson * events[i].persons + bars[j].pricePerHour * events[i].time;
+      if(events[i].persons > 10)
+      {
+        price_c = price_c + price_c*0.1;
+
+      }
+      if(events[i].persons > 20)
+      {
+        price_c = price_c + price_c*0.2;
+      }
+      if(events[i].persons > 30)
+      {
+        price_c = price_c + price_c*0.3;
+
+      }
+      if (events[i].options.deductibleReduction == true)
+      {
+        price_c = price_c + events[i].persons;
+      }
+      
+      events[i].price = price_c;
+
+      var commission_p = 0.3 * price_c;
+      var insurance_p = 0.5 * commission_p;
+
+      events[i].commission.insurance = insurance_p;
+      events[i].commission.treasury = events[i].persons;
+
+      var privateaser_p = commission_p - (events[i].persons + insurance_p);
+      if (privateaser_p<=0)
+      {
+        privateaser_p=0;
+      }
+      events[i].commission.privateaser = privateaser_p;
+    }
+  }
+}
+
+
+actors.forEach(actor => {
+events.forEach(event =>{
+  if(actor.eventId == event.id)
+  {
+    actor.payment[0].amount = event.price;
+    actor.payment[1].amount = event.price - (event.price * 0.3);
+    actor.payment[2].amount = event.commission.insurance;
+    actor.payment[3].amount = event.persons;
+    actor.payment[4].amount = event.commission.privateaser;
+  }
+})
+})
+
+
+
+console.log(events);
+console.log(actors);
